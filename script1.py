ecc8f7fff188e61a4b8172079bc0af603ce74591
onuobodo <onu1606@gmail.com>
2026-06-01 17:05:28 -0400

    put comment back in script 2
    Added text from python

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Special cases aren't special enough to break the rules.")
+print("There should be one-- and preferably only one --obvious way to do it.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("If the implementation is easy to explain, it may be a good idea.")
+print("Although that way may not be obvious at first unless you're Dutch.")
